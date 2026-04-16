0e4024c14b0bca709af7142e2756bf9ff65d04a7
mabuelchuri <133939248+mabuelchuri@users.noreply.github.com>
2026-04-16 09:46:11 +0530

    Create devops_example.py

---
+++ Day01/devops_example.py
@@ -0,0 +1,13 @@
+# devops_examples.py
+
+cpu_usage = "85"
+memory_usage = "70.5"
+
+cpu_usage = int(cpu_usage)
+memory_usage = float(memory_usage)
+
+print(f"CPU Usage: {cpu_usage}%")
+print(f"Memory Usage: {memory_usage}%")
+
+servers = 3
+print(f"Total Servers Running: {servers}")
