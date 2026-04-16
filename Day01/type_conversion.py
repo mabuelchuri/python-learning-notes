30316bd767497d8361f9c215a3026a8f53f348fc
mabuelchuri <133939248+mabuelchuri@users.noreply.github.com>
2026-04-16 09:45:19 +0530

    Create type_conversion.py

---
+++ Day01/type_conversion.py
@@ -0,0 +1,15 @@
+# type_conversion.py
+
+age = "25"
+salary = "5000.5"
+
+age = int(age)
+salary = float(salary)
+
+print(age)
+print(salary)
+
+print(type(age))
+print(type(salary))
+
+print(f"Next year age will be {age + 1}")
