4c1ca638fe2675c964d9815802d915294c209f95
mabuelchuri <133939248+mabuelchuri@users.noreply.github.com>
2026-04-16 09:40:52 +0530

    Create string.py

---
+++ Day01/string.py
@@ -0,0 +1,12 @@
+# strings.py
+
+first_name = "Mabu"
+last_name = "Elchuri"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+message = "Python Learning Journey"
+print(message)
+print(type(message))
