a8e55e491f81e28ab44d3aadc7a8ecbdf295913d
mabuelchuri <133939248+mabuelchuri@users.noreply.github.com>
2026-04-26 19:37:19 +0530

    Create list_methods.py

---
+++ Day 05/list_methods.py
@@ -0,0 +1,15 @@
+# list_methods.py
+
+fruits = ["apple", "banana"]
+
+fruits.append("mango")
+print("After append:", fruits)
+
+fruits.insert(1, "orange")
+print("After insert:", fruits)
+
+fruits.remove("banana")
+print("After remove:", fruits)
+
+fruits.pop()
+print("After pop:", fruits)
